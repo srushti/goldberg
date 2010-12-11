f0d9cd4e3285d2db22c2d769750796bccdfdfa87
Niranjan Paranjape <niranjan@c42.in>
2010-12-11 20:15:06 +0530

    created a group for sinatra called web and using bundle require

--- lib/goldberg.rb
+++ lib/goldberg.rb
@@ -1,16 +1,22 @@
-require 'sinatra'
-require 'git'
+require 'rubygems'
+begin
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
+  require 'bundler'
+  Bundler.setup
+rescue Bundler::GemNotFound => e
+  STDERR.puts e.message
+  STDERR.puts "Try running 'bundle install'."
+  exit!
+end
+Bundler.require(:default)
 
 module Goldberg
 end
 
-['/goldberg/**/*.rb'].each{|directory|
-  Dir["#{File.expand_path(File.dirname(__FILE__) + directory)}"].each { |file|
+['/goldberg/**/*.rb'].each do |directory|
+  Dir["#{File.expand_path(File.dirname(__FILE__) + directory)}"] do { |file|
     require file
-  }
-}
-
-get '/' do
-  'Hello world!'
+  end
 end
+
 
