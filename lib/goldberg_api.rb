dd771d62136ba82fca1a2bbc300c6463e1c1a02e
Srushti Ambekallu <srushti@c42.in>
2011-01-27 14:46:34 +0530

    added status reporting

--- lib/goldberg_api.rb
+++ lib/goldberg_api.rb
@@ -1,11 +1,12 @@
 require 'goldberg'
+require File.join(File.dirname(__FILE__), 'goldberg', 'project')
 
 Bundler.require(:web)
 
 module GoldbergApi
   class Application < Sinatra::Application
     get '/' do
-      'Butterfly'
+      Goldberg::Project.all.each.map{|p| "#{p.name} #{p.status}" }.join(' ')
     end
   end
 end
--- spec/lib/goldberg_api_spec.rb
+++ spec/lib/goldberg_api_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 module GoldbergApi
   describe Application do
     it "should flap wings" do
+      projects = [1, 2].map{|i| mock("project#{i}", :name => "name#{i}", :status => "status#{i}")}
+      Goldberg::Project.should_receive(:all).and_return(projects)
       get '/'
-      last_response.body.should == 'Butterfly'
+      last_response.body.should == "name1 status1 name2 status2"
     end
   end
 end
