77f9025d9fd0f907b3f62598613a08f5874d13e5
Srushti Ambekallu <srushti@c42.in>
2011-02-17 12:35:31 +0530

    fixed builds sorting (it was getting confused with 2-digit numbers)

--- lib/goldberg/build.rb
+++ lib/goldberg/build.rb
@@ -1,11 +1,13 @@
 module Goldberg
   class Build
+    include Comparable
+
     def self.all(project)
       FileUtils.mkdir_p(project.builds_path) if !File.exist?(project.builds_path)
-      (Dir.entries(project.builds_path) - ['.', '..']).sort.reverse
+      (Dir.entries(project.builds_path) - ['.', '..'])
         .map{|entry| File.join(project.builds_path, entry)}
         .select{|entry| File.directory?(entry)}
-        .map{|dir_entry| Build.new(dir_entry)}
+        .map{|dir_entry| Build.new(dir_entry)}.sort.reverse
     end
 
     def self.null
@@ -27,9 +29,18 @@ module Goldberg
     def status
       build_status_path = File.join(@path, 'build_status')
       if File.exist?(build_status_path)
-        Environment.read_file(build_status_path) == 'true' ? 'passed' : 'failed'
+        Environment.read_file(build_status_path) == 'true'
       else
         nil
+      end
+    end
+
+    def <=>(other)
+      path_comparison = build_log_path <=> build_log_path
+      if path_comparison != 0
+        path_comparison
+      else
+        number.to_i <=> other.number.to_i
       end
     end
   end
--- spec/goldberg/build_spec.rb
+++ spec/goldberg/build_spec.rb
@@ -2,5 +2,13 @@ require "spec_helper"
 
 module Goldberg
   describe Build do
+    it "gives all builds" do
+      File.should_receive(:exist?).with('builds_path').and_return(true)
+      Dir.should_receive(:entries).with('builds_path').and_return(['.', '..', '9', '10', '11'])
+      (9..11).each do |i|
+        File.should_receive(:directory?).with("builds_path/#{i.to_s}").and_return(true)
+      end
+      Build.all(mock(Project, :builds_path => 'builds_path')).should =~ [Build.new('builds_path/9'), Build.new('builds_path/10'), Build.new('builds_path/11')]
+    end
   end
 end
