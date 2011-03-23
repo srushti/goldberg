d56f2825e210eaa4bb84e1c945d6f7eccdd0d524
Niranjan Paranjape <niranjan@c42.in>
2011-03-23 19:44:11 +0530

    removing paths which are not used

--- app/models/paths.rb
+++ app/models/paths.rb
@@ -2,14 +2,6 @@ require "fileutils"
 
 module Paths
   class << self
-    def root
-      absolute_path
-    end
-
-    def absolute_path(*args)
-      File.join(File.dirname(__FILE__), %w(.. ..) + args)
-    end
-
     def projects
       goldberg_path = Env['GOLDBERG_PATH'] || File.join(Env['HOME'], '.goldberg')
       create_if_doesnt_exist(File.join(goldberg_path, 'projects'))
