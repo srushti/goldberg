253736421ae53b6d120f391a3db3c8a2148933e3
Srushti Ambekallu <srushti@ambekallu.com>
2011-04-02 21:13:54 +0530

    added warning messages and a bootstrap command for rvm

--- app/models/init.rb
+++ app/models/init.rb
@@ -1,10 +1,29 @@
 class Init
+  def self.allowed_methods
+    ['bootstrap', 'add', 'remove', 'list', 'start', 'start_poller']
+  end
+
   def run
-    if Environment.argv.size > 0 && ['add', 'remove', 'list', 'start', 'start_poller'].include?(Environment.argv[0])
+    if Environment.argv.size > 0 && Init::allowed_methods.include?(Environment.argv[0])
       send(Environment.argv[0])
     else
       Rails.logger.info "You did not pass any command."
-      Rails.logger.info "Valid commands are add, remove, list, start & start_poller."
+      Rails.logger.info "Valid commands are #{Init::allowed_methods.join(', ')}."
+    end
+  end
+
+  def bootstrap
+    if File.exist?(File.join(Env['HOME'], '.rvm', 'scripts', 'rvm'))
+      Rails.logger.info "It looks like you have RVM installed. We will now add the following settings to your global .rvmrc located at #{Env['HOME']}."
+      goldberg_rvmrc_contents =  "rvm_install_on_use_flag=1\nrvm_project_rvmrc=1\nrvm_gemset_create_on_use_flag=1"
+      Rails.logger.info "#{goldberg_rvmrc_contents}\n(Y/n)"
+      if ['yes', 'y'].include?(STDIN.gets.chomp.downcase)
+        File.open(File.join(Env['HOME'], '.rvmrc'), 'a') {|f| f.write(goldberg_rvmrc_contents)}
+      else
+        Rails.logger.info "Aborting"
+      end
+    else
+      Rails.logger.info "RVM doesn't seem to be installed! You can use Goldberg but all projects will be run on the default ruby: #{RUBY_VERSION}. If you wish to run on different rubies install rvm and run this 'bin/goldberg bootstrap' again."
     end
   end
 
