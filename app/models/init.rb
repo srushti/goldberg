31e2218145dd04e2a516550e0fe5b37cbe2bd2c0
aakash <aakashd@gmail.com>
2011-04-11 00:01:36 +0530

    using commander to build the Goldberg command line utility

--- app/models/init.rb
+++ app/models/init.rb
@@ -1,24 +1,11 @@
 class Init
-  def self.allowed_methods
-    ['bootstrap', 'add', 'remove', 'list', 'start', 'start_poller']
-  end
-
-  def run
-    if Environment.argv.size > 0 && Init::allowed_methods.include?(Environment.argv[0])
-      send(Environment.argv[0])
-    else
-      Rails.logger.info "You did not pass any command."
-      Rails.logger.info "Valid commands are #{Init::allowed_methods.join(', ')}."
-    end
-  end
-
   def bootstrap
     if File.exist?(File.join(Env['HOME'], '.rvm', 'scripts', 'rvm'))
       Rails.logger.info "It looks like you have RVM installed. We will now add the following settings to your global .rvmrc located at #{Env['HOME']}."
-      goldberg_rvmrc_contents =  "rvm_install_on_use_flag=1\nrvm_project_rvmrc=1\nrvm_gemset_create_on_use_flag=1"
+      goldberg_rvmrc_contents = "rvm_install_on_use_flag=1\nrvm_project_rvmrc=1\nrvm_gemset_create_on_use_flag=1"
       Rails.logger.info "#{goldberg_rvmrc_contents}\n(Y/n)"
       if ['yes', 'y'].include?(STDIN.gets.chomp.downcase)
-        File.open(File.join(Env['HOME'], '.rvmrc'), 'a') {|f| f.write(goldberg_rvmrc_contents)}
+        File.open(File.join(Env['HOME'], '.rvmrc'), 'a') { |f| f.write(goldberg_rvmrc_contents) }
       else
         Rails.logger.info "Aborting"
       end
@@ -27,33 +14,21 @@ class Init
     end
   end
 
-  def add
-    if Environment.argv.size >= 3
-      Project.add(:url => Environment.argv[1], :name => Environment.argv[2], :command => Environment.argv[3])
-      Rails.logger.info "#{Environment.argv[2]} successfully added."
-    else
-      Rails.logger.info "Usage 'bin/goldberg add <url> <name> [custom_command]'"
-    end
+  def add(url, name, command = nil)
+    Project.add(:url => url, :name => name, :command => command)
+    Rails.logger.info "#{name} successfully added."
   end
 
-  def remove
-    if Environment.argv.size == 2
-      Project.find_by_name(Environment.argv[1]).destroy
-      Rails.logger.info "#{Environment.argv[1]} successfully removed."
-    else
-      Rails.logger.info "Usage 'bin/goldberg remove <name>'"
-    end
+  def remove(name)
+      Project.find_by_name(name).destroy
+      Rails.logger.info "#{name} successfully removed."
   end
 
   def list
-    Project.all.map(&:name).each{|name| Rails.logger.info name}
+    Project.all.map(&:name).each { |name| Rails.logger.info name }
   end
 
-  def start
-    port = 3000
-    if Environment.argv.size == 2
-      port = Environment.argv[1].to_i
-    end
+  def start(port = 3000)
     Environment.exec "rackup -p #{port} #{File.join(File.dirname(__FILE__), '..', '..', 'config.ru')}"
   end
 
--- spec/models/init_spec.rb
+++ spec/models/init_spec.rb
@@ -2,41 +2,38 @@ require "spec_helper"
 
 describe Init do
   it "adds a new project" do
-    Environment.stub!(:argv).and_return(['add', 'url', 'name'])
     Project.should_receive(:add).with(:url => 'url', :name => 'name', :command => nil)
     Rails.logger.should_receive(:info).with('name successfully added.')
-    Init.new.run
+    Init.new.add('url','name')
   end
 
   it "adds a new project with a custom command" do
-    Environment.stub!(:argv).and_return(['add', 'url', 'name', 'cmake'])
     Project.should_receive(:add).with(:url => 'url', :name => 'name', :command => 'cmake')
     Rails.logger.should_receive(:info).with('name successfully added.')
-    Init.new.run
+    Init.new.add('url','name','cmake')
   end
 
   it "removes the specified project" do
-    Environment.stub!(:argv).and_return(['remove', 'name'])
     project = Factory(:project, :name => 'name')
     Rails.logger.should_receive(:info).with('name successfully removed.')
-    Init.new.run
+    Init.new.remove('name')
     Project.find_by_id(project.id).should_not be
   end
 
   it "lists all projects" do
     project = Factory(:project, :name => 'a_project')
-    Environment.stub!(:argv).and_return(['list'])
     Rails.logger.should_receive(:info).with(project.name)
-    Init.new.run
+    Init.new.list
   end
 
-  {(['start']) => 3000, (['start', '9292']) => 9292}.each_pair do |args, port|
-    it "starts the app on port #{port} with arguments #{args}" do
-      Environment.stub!(:argv).and_return(args)
-      app_root = File.join(File.dirname(__FILE__)).split('/spec/goldberg')[0]
-      Environment.should_receive(:exec).with("rackup -p #{port} #{File.join(Rails.root, 'app', 'models', '..', '..', 'config.ru')}")
-      Init.new.run
-    end
+  it "starts the app on port 3000 if no port specified" do
+    Environment.should_receive(:exec).with("rackup -p 3000 #{File.join(Rails.root, 'app', 'models', '..', '..', 'config.ru')}")
+    Init.new.start
+  end
+
+  it "starts the app on specified port if port specified" do
+    Environment.should_receive(:exec).with("rackup -p 4356 #{File.join(Rails.root, 'app', 'models', '..', '..', 'config.ru')}")
+    Init.new.start(4356)
   end
 
   it "continues on with the next project even if one build fails" do
