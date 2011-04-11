661a28b9ce3520e1e8abb9041b48c9bd2d919cd6
Srushti Ambekallu <srushti@ambekallu.com>
2011-04-11 13:42:09 +0530

    forgot to checkin the actual other module

---
+++ app/models/rvm.rb
@@ -0,0 +1,15 @@
+module RVM
+  class << self
+    def installed?
+      File.exist?(File.join(Env['HOME'], '.rvm', 'scripts', 'rvm'))
+    end
+
+    def goldberg_rvmrc_contents
+      "rvm_install_on_use_flag=1\nrvm_project_rvmrc=1\nrvm_gemset_create_on_use_flag=1"
+    end
+
+    def write_goldberg_rvmrc_contents
+      File.open(File.join(Env['HOME'], '.rvmrc'), 'a') {|f| f.write(goldberg_rvmrc_contents)}
+    end
+  end
+end
