f7d7fba6bef2341de712e986b491842c457f59fd
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-04-17 00:13:41 -0700

    Adding time_ago to timestamp, and also using DRY

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
   def project_status(status)
     status.gsub(/ /, '_')
-  end
+  end                  
+  
+  def format_timestamp(timestamp)
+#    haml_tag(:div, :class => 'timestamp') do
+       haml_concat timestamp.strftime("%a, %Y/%m/%d %I:%M%p %Z") 
+       haml_concat "( "
+       haml_concat time_ago_in_words(timestamp)       
+       haml_concat " ago )"         
+#     end
+  end  
 end
