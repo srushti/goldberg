7123c2e75ace3e954b9c825b002fd6276cc41efa
Tejas Dinkar <tejas@gja.in>
2011-04-30 19:48:22 +0530

    Added environment_variables to the config class

--- spec/models/project_config_spec.rb
+++ spec/models/project_config_spec.rb
@@ -8,6 +8,21 @@ describe ProjectConfig do
     it "for build frequency should be 20 seconds" do
       config.frequency.should == 20
     end
+
+    it "environment variables should be an empty hash" do
+      config.environment_variables.should == {}
+      config.environment_string.should == ""
+    end
   end
 
+  context "setting values" do
+    
+    it "should be able to add environment variables" do
+      c = Project.configure do |config|
+        config.environment_variables.update("FOO" => "bar")
+      end
+      c.environment_variables.should == { "FOO" => "bar" }
+      c.environment_string.should == "FOO=bar"
+    end
+  end
 end
