fc67bfde56b5e3f16b3e859d07a90f3d2e49e66c
Srushti Ambekallu <srushti@ambekallu.com>
2011-05-05 10:08:03 +0530

    forgot to fix one test after the rename

--- spec/models/rvm_spec.rb
+++ spec/models/rvm_spec.rb
@@ -4,6 +4,6 @@ describe RVM do
   it "writes the required rvmrc contents" do
     Env.stub!(:[]).with('HOME').and_return('home')
     Environment.should_receive(:write_file).with('home/.rvmrc', "rvm_install_on_use_flag=1\nrvm_project_rvmrc=1\nrvm_gemset_create_on_use_flag=1\n")
-    RVM.write_goldberg_rvmrc_contents
+    RVM.write_ci_rvmrc_contents
   end
 end
