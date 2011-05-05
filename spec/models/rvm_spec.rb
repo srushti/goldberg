7ae7f07d9dc34870d10993777f891b22e62b51ba
Srushti Ambekallu <srushti@ambekallu.com>
2011-05-05 10:16:26 +0530

    and i changed the rvm_project_rvmrc setting, and forgot to fix the spec

--- spec/models/rvm_spec.rb
+++ spec/models/rvm_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe RVM do
   it "writes the required rvmrc contents" do
     Env.stub!(:[]).with('HOME').and_return('home')
-    Environment.should_receive(:write_file).with('home/.rvmrc', "rvm_install_on_use_flag=1\nrvm_project_rvmrc=1\nrvm_gemset_create_on_use_flag=1\n")
+    Environment.should_receive(:write_file).with('home/.rvmrc', "rvm_install_on_use_flag=1\nrvm_project_rvmrc=0\nrvm_gemset_create_on_use_flag=1\n")
     RVM.write_ci_rvmrc_contents
   end
 end
