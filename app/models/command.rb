0ed08a1925a2eaa8efd9cfec5a72b79edfb76976
Srushti Ambekallu <srushti@ambekallu.com>
2011-05-05 11:20:33 +0530

    refactored command to simplify the code a little bit

--- app/models/command.rb
+++ app/models/command.rb
@@ -8,18 +8,18 @@ class Command
   end
 
   def execute_async(total_time)
-    @io = IO.popen(%{/usr/bin/env bash -c "#{command.gsub(/"/, '\"')}"})
+    @pid = IO.popen(%{/usr/bin/env bash -c "#{command.gsub(/"/, '\"')}"}).pid
   end
 
   def running?
-    Process.getpriority(Process::PRIO_PROCESS, @io.pid)
+    Process.getpriority(Process::PRIO_PROCESS, @pid)
     true
   rescue
     false
   end
 
   def kill
-    Process.kill(0, @io.pid)
+    Process.kill(0, @pid)
   end
 
   def self.success?
