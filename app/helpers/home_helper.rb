880580ba4d4b67815aeb650b6003c2f62ddd229e
Rohit Arondekar <rohit.arondekar@gmail.com>
2011-05-12 04:44:52 -0700

    Remove unnecessary filename manipulation as correct name is provided.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
   def project_status_image(status)
-    if ['building', 'not available', 'cancelled'].include?(status)
-      image_tag "#{status.gsub(/ /, '_')}.gif", :alt => status, :title => status
+    if ['building', 'not_available', 'cancelled'].include?(status)
+      image_tag "#{status}.gif", :alt => status, :title => status
     else
       image_tag "#{status}.png", :alt => status, :title => status
     end
