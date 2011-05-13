338782cfa0c089a488a8b840a672204f603e1899
Srushti Ambekallu <srushti@ambekallu.com>
2011-05-13 22:41:29 +0530

    fixed the specs to check for checkout instead of 'co'

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -6,12 +6,12 @@ describe Repository do
     Environment.should_receive(:system).with("git clone --depth 1 url code_path --branch production").and_return(true)
     repo.checkout.should be_true
   end
-  
+
   it "knows the sha of current revision" do
     Environment.should_receive(:system_call_output).with("cd code_path && git rev-parse --verify HEAD").and_return("random_sha\n")
     repo.revision.should == "random_sha"
   end
-  
+
   context "update" do
     it "updates the code at given location and return true if there are updates" do
       Environment.should_receive(:system_call_output).with("cd code_path && git rev-parse --verify HEAD").and_return("old_sha")
@@ -19,7 +19,7 @@ describe Repository do
       Environment.should_receive(:system).with("cd code_path && git pull").and_return(true)
       repo.update.should be_true
     end
-    
+
     it "returns false if code there were no updates" do
       Environment.should_receive(:system_call_output).with("cd code_path && git rev-parse --verify HEAD").and_return("old_sha")
       Environment.should_receive(:system_call_output).with("cd code_path && git rev-parse --verify HEAD").and_return("old_sha")
@@ -27,18 +27,18 @@ describe Repository do
       repo.update.should be_false
     end
   end
-  
+
   context "change list" do
     it "retrieves a list of changes given two revisions" do
       Environment.should_receive(:system_call_output).with("cd code_path && git whatchanged old_sha..new_sha --pretty=oneline --name-status").and_return("change list")
       repo.change_list("old_sha", "new_sha").should == "change list"
     end
-    
+
     it "returns empty change list if old sha is blank" do
       Environment.should_not_receive(:system_call_output)
       repo.change_list(nil, "new_sha").should be_blank
     end
-    
+
     it "retrieves a list of changes if old sha is present but new sha is blank" do
       Environment.should_receive(:system_call_output).with("cd code_path && git whatchanged old_sha.. --pretty=oneline --name-status").and_return("change list")
       repo.change_list("old_sha", nil).should == "change list"
@@ -47,12 +47,12 @@ describe Repository do
 
   describe "check if file is versioned" do
     it "should return false if the file is not versioned" do
-      Environment.should_receive(:system_call_output).with("cd code_path && git co some_file 2>>/dev/null || echo 'not versioned'").and_return("not versioned")
+      Environment.should_receive(:system_call_output).with("cd code_path && git checkout some_file 2>>/dev/null || echo 'not versioned'").and_return("not versioned")
       repo.versioned?("some_file").should be_false
     end
-    
+
     it "should return true if the file is versioned" do
-      Environment.should_receive(:system_call_output).with("cd code_path && git co some_file 2>>/dev/null || echo 'not versioned'").and_return("")
+      Environment.should_receive(:system_call_output).with("cd code_path && git checkout some_file 2>>/dev/null || echo 'not versioned'").and_return("")
       repo.versioned?("some_file").should be_true
     end
   end
