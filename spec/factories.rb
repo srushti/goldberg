8dbd542378b1cdf494e1049b5101987411f32baf
David Barral <david.barral@trabesoluciones.com>
2011-06-20 15:36:28 +0200

    Status mail body link did not point to the correct build url

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,10 @@ Factory.sequence :project_name do |n|
   "project_#{n}"
 end
 
+Factory.sequence :build_number do |n|
+  n
+end
+
 Factory.define :project do |p|
   p.name { Factory.next(:project_name) }
   p.url  { |project| "git://domain/#{project.name}.git" }
@@ -10,6 +14,7 @@ end
 
 Factory.define :build do |b|
   b.project { Factory(:project) }
+  b.number  { Factory.next(:build_number) }
   b.revision "some_random_sha"
 end
 
