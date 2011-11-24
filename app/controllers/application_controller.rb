2e0ba123b1cc1a57282d2b7d2bf866f0234e6f77
Arvind Laxminarayan <ardsrk@gmail.com>
2011-11-24 23:43:07 +0530

    added user signin with github oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user
+
+  private
+  def current_user
+    session['user']
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    redirect_to "#{GITHUB_CONFIG['authorize_url'] % GITHUB_CONFIG['client_id']}"
+  end
+
+  def signout
+    session.delete('user')
+    redirect_to root_path
+  end
+
+  def github_callback
+    access_token = Github.get_oauth_token(params['code'])
+    if access_token
+      session[:access_token] = access_token
+      session[:user] = Github.get_login(access_token)
+    end
+    redirect_to root_path
+  end
+end
