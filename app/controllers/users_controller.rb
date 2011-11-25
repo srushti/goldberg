389931483f6abe276f22087715bf622a9a8f880f
Arvind Laxminarayan <ardsrk@gmail.com>
2011-11-25 10:53:34 +0530

    don't store access token in session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
   def github_callback
     access_token = Github.get_oauth_token(params['code'])
     if access_token
-      session[:access_token] = access_token
       session[:user] = Github.get_login(access_token)
     end
     redirect_to root_path
