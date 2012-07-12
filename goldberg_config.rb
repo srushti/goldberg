34efb397cfe6d1bd440d64404e7596ab1220cd56
Srushti Ambekallu <srushti@ambekallu.com>
2012-07-12 16:04:50 +0530

    adding JRUBY_OPTS=--1.9 for the jruby build

--- goldberg_config.rb
+++ goldberg_config.rb
@@ -2,5 +2,5 @@ Project.configure do |config|
   config.group = 'c42'
   config.rake_task = 'ci'
   config.use_bundle_exec = true
-  config.environment_variables = {'RAILS_ENV' => 'test'}
+  config.environment_variables = { 'RAILS_ENV' => 'test', 'JRUBY_OPTS' => '--1.9' }
 end
