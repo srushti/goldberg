23ea9a58c85dbd0cc080dd94077486df4ffa6803
Srushti Ambekallu <srushti@ambekallu.com>
2012-08-01 20:29:06 +0530

    Using just the first & last revisions while getting the svn range (not ever individual one).

--- app/models/scm/svn.rb
+++ app/models/scm/svn.rb
@@ -4,25 +4,25 @@ module Scm
       def revision
         "svnversion ."
       end
-      
+
       def checkout(url, code_path, branch)
         "svn co #{url} #{code_path}"
       end
-      
+
       def update(branch)
         "svn revert -R . && svn update"
       end
-      
+
       def change_list(old_rev, new_rev)
         "svn diff -r#{old_rev}:#{new_rev} | diffstat"
       end
-      
+
       def version(file_path)
         "svn info #{file_path}"
       end
 
       def authors(versions)
-        "svn log -r#{versions.join(':')} | grep \"^[r\d]\" | awk '{print $3}'| uniq| tr \"\\n\" \" \""
+        "svn log -r#{versions.first+':'+versions.last} | grep \"^[r\d]\" | awk '{print $3}'| uniq| tr \"\\n\" \" \""
       end
     end
   end
